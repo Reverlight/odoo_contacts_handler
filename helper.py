9f146c4fe25f695ad60bc14da19d3fce0464cf9c
Artem Jheleznov <artem3766@gmail.com>
2023-12-10 15:00:01 +0300

    Add helper

---
+++ helper.py
@@ -0,0 +1,4 @@
+def format_dict_to_list(modified):
+    # Format dict to be list of it's values
+    modified_values = [modified_single for modified_single in modified.values()]
+    return modified_values
